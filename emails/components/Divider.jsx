2595ee3e914f46dce3c21f78de889a91031d405b
Peter Sugihara <peter@campsh.com>
2022-10-28 22:29:57 -0700

    Divider

---
+++ emails/components/Divider.jsx
@@ -0,0 +1,12 @@
+import { colors } from "./theme";
+
+export default function Divider() {
+  return (
+    <MjmlDivider
+      borderColor={colors.yellow}
+      paddingTop="48px"
+      paddingBottom="36px"
+      borderWidth="6px"
+    />
+  );
+}
